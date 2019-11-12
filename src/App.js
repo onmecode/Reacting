791d3d7a23ae55f670f59b6b2b0baf9a27a07ada
System Administrator <onmecode@gmail.com>
2019-11-12 23:42:10 +0000

    abc reacting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import Tweet from "./tweet";
+import "./App.css";
+import "./index";
 function App() {
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  };
+  const [users, setUsers] = useState([
+    { name: "Gaia", message: "Hello I am Gaia" },
+    { name: "Ire", message: "Hello I am Irene" },
+    { name: "Gemma", message: "Hello I am Gemma" }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className={isRed ? "red" : ""}>Change my color</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
