20f942d283c046290fc467d26ed12cb0e9a998e7
System Administrator <onmecode@gmail.com>
2019-11-20 23:27:23 +0000

    fetching data - displaying weather conditions - handling error for empty values

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,7 +2,24 @@ import React from "react";
 
 class Weather extends React.Component {
   render() {
-    return <div>Weather Component</div>;
+    return (
+      <div>
+        {this.props.city && this.props.country && (
+          <p className="weather__key">
+            {" "}
+            Location:
+            <span className="weather__value">
+              {" "}
+              {this.props.city}, {this.props.country}
+            </span>
+          </p>
+        )}
+        {this.props.temp && <p>Temperature: {this.props.temp}</p>}
+        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+        {this.props.description && <p>Conditions: {this.props.description}</p>}
+        {this.props.error && <p> Ops: {this.props.error}</p>}
+      </div>
+    );
   }
 }
 
